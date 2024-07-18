23ad70fcad07643295a9bd9afa4062ced35e15ab
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-07-18 11:47:28 +0300

    feat(chatlayout): add conversationitem

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,3 +1,6 @@
+import ConversationItem from "@/Components/App/ConversationItem";
+import TextInput from "@/Components/TextInput";
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
 import { usePage } from "@inertiajs/react";
 import React, { useEffect, useState } from "react";
 
@@ -9,33 +12,42 @@ const ChatLayout = ({ children }) => {
     const [localConversations, setLocalConversations] = useState([]);
     const [sortedConversations, setSortedConversations] = useState([]);
 
-
     const isUserOnline = (userId) => onlineUsers[userId];
+    
     console.log("Conversation:", conversations);
     console.log("Selected Conversation:", selectedConversation);
 
+    const onSearch = (ev) => {
+        const search = ev.target.value.toLowerCase();
+        setLocalConversations(
+            conversations.filter((conversation) => {
+                return conversation.name.toLowerCase().includes(search);
+            })
+        );
+    }
 
     useEffect(() => {
         setSortedConversations(
             localConversations.sort((a, b) => {
                 if (a.blocked_at && b.blocked_at) {
                     return a.blocked_at > b.blocked_at ? 1 : -1;
-                }else if (a.blocked_at) {
+                } else if (a.blocked_at) {
                     return 1;
-                }else if (b.blocked_at) {
+                } else if (b.blocked_at) {
                     return -1;
                 }
 
-                if(a.last_message_date && b.last_message_date){
-                    return b.last_message_date.localeCompare(a.last_message_date);
-                }else if(a.last_message_date){
-                    return -1;  
-                }else if(b.last_message_date){
+                if (a.last_message_date && b.last_message_date) {
+                    return b.last_message_date.localeCompare(
+                        a.last_message_date
+                    );
+                } else if (a.last_message_date) {
+                    return -1;
+                } else if (b.last_message_date) {
                     return 1;
-                }else{
+                } else {
                     return 0;
                 }
-
             })
         );
     }, [localConversations]);
@@ -43,7 +55,6 @@ const ChatLayout = ({ children }) => {
     useEffect(() => {
         setLocalConversations(conversations);
     }, [conversations]);
-
 
     useEffect(() => {
         Echo.join("online")
@@ -80,7 +91,44 @@ const ChatLayout = ({ children }) => {
 
     return (
         <>
-            
+            <div className="flex-1 w-full flex overflow-hidden">
+                <div
+                    className={`transition-all w-full sm:w-[220px] md:w-[300px] bg-slate-800 flex flex-col overflow-hidden ${
+                        selectedConversation ? "-ml-[100%] sm:ml-0" : ""
+                    }`}
+                >
+                    <div className="flex items-center justify-between py-2 px-3 text-xl font-medium">
+                        My Conversations
+                        <div className="tooltip tooltip-left" data-tip="Create new Group">
+                            <button className="text-gray-400 hover:text-gray-200">
+                                <PencilSquareIcon className="h-4 w-4 inline-block ml-2" />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="p-3">
+                        <TextInput 
+                            onKeyUp={onSearch}
+                            placeholder="Filter users and groups"
+                            className="w-full"
+                        />
+                    </div>
+                    <div className="flex-1 overflow-auto">
+                        {sortedConversations &&
+                            sortedConversations.map((conversation) => (
+                                <ConversationItem 
+                                    key={`${conversation.is_group ? "group_" : "user_"}${conversation.id}`}
+                                    conversation={conversation}
+                                    online = {!!isUserOnline(conversation.id)}
+                                    selectedConversation = {selectedConversation}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    {children}
+                </div>
+            </div>
         </>
     );
 };
