d4083fdcca45f22fc491809819520ca8a597ceec
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-07-18 11:49:27 +0300

    feat(web): user and group route define

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ use App\Http\Controllers\HomeController;
 
 Route::middleware(['auth', 'verified'])->group(function() {
     Route::get('/', [HomeController::class, 'home'])->name('dashboard');
+
+
+    Route::get('/user/{user}', function () {
+        
+    })->name('chat.user');
+    Route::get('/group/{group}', function () {
+
+    })->name('chat.group');
 });
 
 Route::middleware('auth')->group(function () {
