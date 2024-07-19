af557f360b59ca03680eff7ecf07758a25f2cf68
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-07-19 12:52:16 +0300

    feat(group): updategroupwithmessage function cereate and update

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -61,4 +61,12 @@ class Group extends Model
             'last_message_date' => $this->last_message_date,
         ];
     }
+
+    public static function updateGroupWithMessage($groupId, $message)
+    {
+        return self::updateOrCreate(
+            ['id' => $groupId],
+            ['last_message_id' => $message->id]
+        );
+    }
 }
