9bc42171259e844a8310481abe957b13562619a7
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-07-19 10:58:51 +0300

    fix(web): message route create and add

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,17 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MessageController;
 
 Route::middleware(['auth', 'verified'])->group(function() {
     Route::get('/', [HomeController::class, 'home'])->name('dashboard');
 
+    Route::get('user/{user}', [MessageController::class, 'byUser'])->name('chat.user');
+    Route::get('group/{group}', [MessageController::class, 'byGroup'])->name('chat.group');
+    Route::post('/message', [MessageController::class, 'store'])->name('message.store');
+    Route::delete('/message/{message}', [MessageController::class, 'destroy'])->name('message.destroy');
+    Route::get('/message/older/{message}', [MessageController::class, 'loadOlder'])->name('message.loadOlder');
 
-    Route::get('/user/{user}', function () {
-        
-    })->name('chat.user');
-    Route::get('/group/{group}', function () {
-
-    })->name('chat.group');
 });
 
 Route::middleware('auth')->group(function () {
