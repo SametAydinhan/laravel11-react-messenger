da5dd3be097b8bbc56bac4a89845f8f0589ac221
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-07-19 12:53:44 +0300

    feat(channels):add messageuser and messagegroup broadcast channel

--- routes/channels.php
+++ routes/channels.php
@@ -1,8 +1,18 @@
 <?php
 
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('online', function ($user) {
+Broadcast::channel('online', function (User $user) {
     return $user ? new UserResource($user) : null;
+});
+
+
+Broadcast::channel('message.user.{userId1}-{userId2}', function (User $user, int $userId1, int $userId2) {
+    return $user->id === $userId1 || $user->id === $userId2 ? $user : null;
+});
+
+Broadcast::channel('message.group.{groupId}', function (User $user, int $groupId) {
+    return $user->groups->contains('id', $groupId) ? $user : null;
 });
