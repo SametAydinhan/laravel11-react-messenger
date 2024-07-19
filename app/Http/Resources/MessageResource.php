41a43428f176d1557b4e455c276e92ebfa760a7a
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-07-19 10:57:32 +0300

    feat(messageresource): create and update messageresource

---
+++ app/Http/Resources/MessageResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\MessageAttachment;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageResource extends JsonResource
+{
+    public static $wrap = false;
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'message' => $this->message,
+            'sender_id' => $this->sender_id,
+            'receiver_id' => $this->receiver_id,
+            'sender' => new UserResource($this->sender),
+            'group_id' => $this->group_id,
+            'attachment' => MessageAttachmentResource::collection($this->attachments),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
