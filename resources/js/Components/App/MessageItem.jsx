2fd3316cb117ee818011ad2fd7f48f57490cb6de
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-07-19 16:14:52 +0300

    feat(messageıtem):messageıtem component create and update

---
+++ resources/js/Components/App/MessageItem.jsx
@@ -0,0 +1,45 @@
+import { usePage } from "@inertiajs/react";
+import ReactMarkDown from "react-markdown";
+import React from "react";
+import UserAvatar from "./UserAvatar";
+import { formatMessageDateLong } from "@/helper";
+
+const MessageItem = ({message}) => {
+    const currentUser = usePage().props.auth.user
+    return (
+    <div className={
+        "chat " + 
+        (message.sender_id === currentUser.id
+            ? "chat-end"
+            : "chat-start")
+    }>
+        <UserAvatar user={message.sender} />
+        <div className="chat-header">
+            {message.sender_id !== currentUser.id 
+            ? message.sender.name
+            : ""
+        }
+        <time className="text-xs opacity-50 ml-2">
+            {formatMessageDateLong(message.created_at)}
+        </time>
+        </div>
+        <div className={"chat-bubble relative " + 
+            (message.sender_id === currentUser.id
+                ? "chat-bubble-info"
+                : "")
+        }>
+            <div className="chat-message">
+                <div className="chat-message-content">
+                    <ReactMarkDown>
+                        {message.message}
+                    </ReactMarkDown>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default MessageItem
+
