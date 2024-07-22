2f9bcf940a2c2d16e98bb4dc72c574e1596a65fb
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-07-22 10:10:06 +0300

    feat(storeMessageRequest):add rules function

--- app/Http/Requests/StoreMessageRequest.php
+++ app/Http/Requests/StoreMessageRequest.php
@@ -11,7 +11,7 @@ class StoreMessageRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class StoreMessageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'message' => 'nullable|string',
+            'group_id' => 'required_without:receiver_id|nullable|exists:groups,id',
+            'receiver_id' => 'required_without:group_id|nullable|exists:users,id',
+            'attachment' => 'nullable|array|max:10',
+            'attachment.*' => 'file|max:1024000',
         ];
     }
 }
