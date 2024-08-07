722059d2f60238280c6e88c4b43908bfdc4ce85c
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-08-07 18:07:23 +0300

    fix(messageattachmentresource): add wrap

--- app/Http/Resources/MessageAttachmentResource.php
+++ app/Http/Resources/MessageAttachmentResource.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Storage;
 
 class MessageAttachmentResource extends JsonResource
 {
+    public static $wrap = false;
     /**
      * Transform the resource into an array.
      *
